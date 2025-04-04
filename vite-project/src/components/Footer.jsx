d4d40fd5129e9e24da8f746520a75c9e3db3c20c
7682829 <arunimamehta01325@gmail.com>
2025-04-04 13:13:12 +0530

    intial commit for website

---
+++ vite-project/src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { assets } from '../assets/assets/frontend_assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+            <div>
+                <img src={assets.logo} className='mb-5 w-32' alt="" />
+                <p className='w-full md:w-2/3 text-gray-600'>
+                сука блять!
+                </p>
+            </div>
+
+            <div>
+                <p className='text-xl font-medium mb-5'>Company</p>
+                <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+                </ul>
+            </div>
+
+            <div>
+                <p className='text-xl font-medium mb-5'>Connect with Us</p>
+                <ul className='flex flex-col  gap-1 text-gray-600'>
+                    <li>+420 678-456-123</li>
+                    <li>support@forever.com</li>
+                </ul>
+            </div>
+
+            <div className='col-span-full w-full flex flex-col items-center'>
+                <hr className='w-full my-4'/>
+                <p className='py-5 text-sm text-center' > Copyright 2025 @forever.com - All Rights Reserved.</p>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Footer;
