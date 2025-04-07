cd3e9aae83c8d5d3a1241227bafa02e6d56ca9df
7682829 <arunimamehta01325@gmail.com>
2025-04-08 01:17:58 +0530

    fixed an error in products.jsx

--- vite-project/src/pages/Product.jsx
+++ vite-project/src/pages/Product.jsx
@@ -7,13 +7,14 @@ const Product = () => {
   const {productId} = useParams();
   const {products} = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
-  const [image, setImage] = useState('');
+  const [image, setImage] = useState('')
 
   const fetchProductData = async () => {
     products.map((item)=>{
       if(item._id === productId){
         setProductData(item)
         setImage(item.image[0])
+        console.log(item);
         return null;
       }
 
@@ -36,14 +37,14 @@ const Product = () => {
           <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
             {
               productData.image.map ((item,index)=>(
-                <img src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" />
+                <img onClick={()=>setImage(item)}  src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" />
 
               ))
             }
 
           </div>
           <div className='w-full sm:w-[80%]'>
-            <img onClick={()=>setImage(item)} className='w-full h-auto' src={image} alt="" />  
+            <img className='w-full h-auto' src={image} alt="" />  
 
           </div>
         </div>
