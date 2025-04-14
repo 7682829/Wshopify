3d67fae507032d9decabc1014f5f1b92bf10f00c
7682829 <arunimamehta01325@gmail.com>
2025-04-14 17:10:21 +0530

    finished backend

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
     const productSchema = new mongoose.Schema({
         name: { type: String, required: true }, 
         description: { type: String, required: true }, 
-        backprice: { type: Number, required: true }, 
         image: { type: Array, required: true }, // Fixed here
         category: { type: String, required: true }, 
         subCategory: { type: String, required: true }, 
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
 
 // app config
 const app=express()
@@ -17,12 +18,12 @@ app.use(cors())
 
 //api endpoints
 app.use('/api/user',userRouter)
+app.use('/api/product',productRouter)
 
 app.get('/',(req,res)=>{
 	res.send("API Working")
 })
  
-
 
 app.listen(port, ()=> console.log('Server started on PORT: ' +port))
 
