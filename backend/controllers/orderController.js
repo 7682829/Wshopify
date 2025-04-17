3604ba28785cc4076ea60bcbef5ee73a9a99ed86
7682829 <arunimamehta01325@gmail.com>
2025-04-17 06:26:51 +0530

    finished until 11hrs 11min 37secs

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,80 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+
+
+//Placing orders using COD method
+const placeOrder = async (req,res) => {
+    try {
+
+        const {userId, items, amount, address} = req.body;
+
+        const orderData = {
+            userId, 
+            items,
+            address, 
+            amount, 
+            paymentMethod: "COD", 
+            payment:false, 
+            date: Date.now()  
+        }
+
+        const newOrder = new orderModel(orderData)
+        await newOrder.save()
+
+        await userModel.findByIdAndUpdate(userId,{cartData:{}})
+
+        res.json({success:true, message: "Order Placed!"})
+    } 
+    catch (error) 
+    {
+        console.log(error);
+        res.json({success:false, message: error.message})
+    }
+
+}
+
+//Placing orders using Stripe method
+const placeOrderStripe = async (req,res) => {
+
+}
+
+//Placing orders using Razorpay method
+const placeOrderRazorpay = async (req,res) => {
+
+}
+
+//All orders data for Admin Panel 
+const allOrders = async (req,res) => {
+
+}
+
+//User order data for Frontend
+const userOrders = async (req,res) => {
+
+}
+
+//Update order status from Admin Panel
+const updateStatus = async (req,res) => {
+
+}
+
+export {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- vite-project/src/context/ShopContext.jsx
+++ vite-project/src/context/ShopContext.jsx
@@ -39,9 +39,19 @@ const ShopContextProvider = (props) => {
             cartData[itemId][size] = 1;
         }
         setCartItems(cartData);
+
+        if (token) {
+            try {
+
+                await axios.post(backendUrl + '/api/cart/add', {itemId, size}, {headers:{token}})
+                
+            } 
+            catch (error) {
+                console.log(error);
+                toast.error(error.message)
+            }
+        }
     }
-
-
 
     const getCartCount = () => {
         let totalCount = 0;
@@ -64,6 +74,16 @@ const ShopContextProvider = (props) => {
             let cartData = structuredClone(cartItems);
             cartData[itemId][size] = quantity;
             setCartItems(cartData);
+
+            if (token) {
+                try {
+                    await axios.post(backendUrl + '/api/cart/update', {itemId, size, quantity}, {headers:{token}}) 
+                } 
+                catch (error) {
+                    console.log(error);
+                    toast.error(error.message)
+                }
+            }
     }
 
     const getCartAmount = () => {
@@ -99,10 +119,30 @@ const ShopContextProvider = (props) => {
             }
     }
 
+    const getUserCart = async (token) => {
+        try {
+            const response = await axios.post(backendUrl + '/api/cart/get',{}, {headers:{token}})
+            if (response.data.success) {
+                setCartItems(response.data.cartData)  
+            }
+        } 
+        catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
 
     useEffect(() => {
         getProductsData()
     }, [])
+
+    useEffect(() => {
+        if (!token && localStorage.getItem('token')) 
+        {
+            setToken(localStorage.getItem('token'))
+            getUserCart(localStorage.getItem('token'))
+        }
+    },[])
 
 
     const value = { 
