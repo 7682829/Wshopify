70adcd49583668633a0a928f48e66fc941aa3650
Sahana257 <sahana.tvsathya@gmail.com>
2025-04-18 21:10:57 +0530

    lol

--- vite-project/src/pages/PlaceOrder.jsx
+++ vite-project/src/pages/PlaceOrder.jsx
@@ -64,6 +64,37 @@ const PlaceOrder = () => {
           }
           break;
 
+
+          case 'stripe':
+
+          const responseStripe = await axios.post(backendUrl + '/api/order/stripe', orderData, {headers: {token}})
+
+          if (responseStripe.data.success) {
+            const {session_url} = responseStripe.data
+            window.location.replace(session_url)
+          } else{
+            toast.error(responseStripe.data.message)
+          }
+
+
+
+          break;
+
+          case 'razorpay':
+
+          const responseRazorpay = await axios.post(backendUrl + '/api/order/razorpay', orderData, {headers:{token}})
+          if (responseRazorpay.data.success) {
+            console.log(responseRazorpay.data.order)
+          }
+
+
+
+          break;
+
+
+
+
+
         default:
           break; 
       }
