07729e2ec383e4333e8307bd358c42f1578bf655
Sahana257 <sahana.tvsathya@gmail.com>
2025-04-19 00:45:31 +0530

    haha

--- vite-project/src/pages/PlaceOrder.jsx
+++ vite-project/src/pages/PlaceOrder.jsx
@@ -26,6 +26,24 @@ const PlaceOrder = () => {
     const value = event.target.value 
     setFormData(data => ({...data,[name]:value}))
   }
+
+  const initPay =(order)=>{
+    const options ={
+      key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+      amount: order.amount,
+      currency: order.currency,
+      name: 'Order Payment',
+      description: 'Order Payment',
+      order_id: order.id,
+      receipt: order.receipt,
+      handler: async(response)=>{
+          console.log(response)
+      }
+    }
+    const rzp = new window.Razorpay(options)
+    rzp.open()
+  }
+
   const onSubmitHandler = async (event) => {
     event.preventDefault()
     try {
