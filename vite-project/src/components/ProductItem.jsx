fef07ed8d117dd085cdc102afb9721f46e08b0eb
Vineela304 <vineelakallepalli@gmail.com>
2025-08-26 02:21:43 +0530

    refinement of collection page logic

--- vite-project/src/components/ProductItem.jsx
+++ vite-project/src/components/ProductItem.jsx
@@ -15,9 +15,9 @@ const ProductItem = ({id, image, name, price}) => {
     };
 
     return (
-        <Link className='text-gray-700 cursor-pointer relative' to={`/product/${id}`}>
-            <div className='overflow-hidden'>
-                <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt="" />
+        <Link className='text-gray-700 cursor-pointer relative h-full flex flex-col' to={`/product/${id}`}>
+            <div className='overflow-hidden aspect-[3/4] mb-3'>
+                <img className='hover:scale-110 transition ease-in-out w-full h-full object-cover' src={image[0]} alt="" />
             </div>
             <button 
                 onClick={handleWishlistClick}
@@ -31,8 +31,10 @@ const ProductItem = ({id, image, name, price}) => {
                     <path d="m12.75 20.66 6.184-7.098c2.677-2.884 2.559-6.506.754-8.705-.898-1.095-2.206-1.816-3.72-1.855-1.293-.034-2.652.43-3.963 1.442-1.315-1.012-2.678-1.476-3.973-1.442-1.515.04-2.825.76-3.724 1.855-1.806 2.201-1.915 5.823.772 8.706l6.183 7.097c.19.216.46.34.743.34a.985.985 0 0 0 .743-.34Z"/>
                 </svg>
             </button>
-            <p className='pt-3 pb-1 text-sm'>{name}</p>
-            <p className='text-sm font-medium'>{currency}{price}</p>
+            <div className="flex-grow">
+                <p className='pt-3 pb-1 text-sm'>{name}</p>
+                <p className='text-sm font-medium'>{currency}{price}</p>
+            </div>
         </Link>
     )
 }
